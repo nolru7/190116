e159ca62cd0cf728d9cae329a60c78e55cb89adc
석훈 이 <nolru7@naver.com>
2019-01-16 11:05:41 +0900

    commit

---
+++ Solution/ClassLibrary1/Database.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1
+{
+    public class Database
+    {
+        private string strConnection1 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.124", "root", "1234", "test");
+        private string strConnection2 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.149", "root", "1234", "gudi");
+
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+                conn.ConnectionString = strConnection1;
+                conn.Open();
+                return conn;
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Solution/WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ClassLibrary1;
+using MySql.Data.MySqlClient;
+
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Class1 c1 = new Class1();
+            Console.WriteLine(c1.GetInt());
+            Database db = new Database();
+
+            MySqlConnection conn = db.GetConnection();
+            if (conn == null)
+            {
+                Console.WriteLine("false!");
+            }
+            else
+            {
+                Console.WriteLine("success!");
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
